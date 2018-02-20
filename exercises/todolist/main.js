a7e8a973db55a0a1eec0ac5c19095564b4735269
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2018-02-20 21:29:07 +0100

    Added button to show storage

--- exercises/todolist/main.js
+++ exercises/todolist/main.js
@@ -3,7 +3,7 @@ const entryPoint = document.getElementById('start');
 function startExercise() {
   // Had problems iterating through getElementByClassName NodeList
   entryPoint.innerHTML +=
-    "<input id='target' type='text' placeholder='Add Item'> <button onclick='getData()'>Add Item</button> ";
+    "<input id='target' type='text' placeholder='Add Item'><button onclick='getData()'>Add Item</button><button onclick='showStorage()'>Show Storage</button><button onclick='clearStorage()'>Clear Storage</button>";
 
   return entryPoint;
 }
@@ -15,6 +15,24 @@ function getData() {
     entryPoint.innerHTML += `<p>${value}</p>`;
     console.log(value);
   }
+}
+
+function clearStorage() {
+  localStorage.clear();
+  console.log('Cleared Storage');
+}
+
+function showStorage() {
+  entryPoint.outerHTML += "<div id='localStorage'></div>";
+  // if (typeof Storage !== 'undefined') {
+  //   // Store
+  //   localStorage.setItem('lastname', 'Smith');
+  //   // Retrieve
+  //   document.getElementById('localStorage').innerHTML = localStorage.getItem('lastname');
+  // } else {
+  //   document.getElementById('localStorage').innerHTML =
+  //     'Sorry, your browser does not support Web Storage...';
+  // }
 }
 
 startExercise();
