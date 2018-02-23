59912be190a02e671725bb18269d2db581d6fb4c
Adrian Abreu <me@adrianabreu.com>
2018-02-23 19:58:57 +0000

    Added jasmine and a example test

---
+++ exercises/example/example.js
@@ -0,0 +1,11 @@
+
+
+// Create a program that tells you if a number is pair or even
+function isPair(number) {
+    return number % 2 == 0;
+}
+
+// This line allows to export on node module and acts as guard for the browser
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = isPair;
+}
