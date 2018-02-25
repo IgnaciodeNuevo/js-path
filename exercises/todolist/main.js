f94641152039fa52e30a65a1af024ae06cb20fad
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2018-02-25 01:46:23 +0100

    Todo list step 2

--- exercises/todolist/main.js
+++ exercises/todolist/main.js
@@ -2,19 +2,40 @@ const entryPoint = document.getElementById('start');
 
 function startExercise() {
   // Had problems iterating through getElementByClassName NodeList
-  entryPoint.innerHTML +=
-    "<input id='target' type='text' placeholder='Add Item'><button onclick='getData()'>Add Item</button><button onclick='showStorage()'>Show Storage</button><button onclick='clearStorage()'>Clear Storage</button>";
+  entryPoint.innerHTML += `
+    <input id='target' type='text' placeholder='Add Item'>
+    <button onclick='addItem()'>Add Item</button>
+    <button onclick='showStorage()'>Show Storage</button>
+    <button onclick='clearStorage()'>Clear Storage</button>
+    <fieldset id="myItems">
+      <legend>Todo List:</legend>
+    </fieldset>`;
 
   return entryPoint;
 }
 
-function getData() {
+function addItem() {
+  const myItems = document.getElementById('myItems');
   let value = document.getElementById('target').value;
+  let hash = Math.round(Math.random() * 1000000000000000000000);
+
+  (function trimText() {
+    return (trimedText = value.replace(/ /g, '') + hash);
+  })();
 
   if (value !== '') {
-    entryPoint.innerHTML += `<p>${value}</p>`;
-    console.log(value);
+    myItems.innerHTML += `
+    <label for="${trimedText}">
+      <input type="checkbox" id="${trimedText}">
+      ${value}
+    </label>`;
   }
+
+  (function clearInut() {
+    if (target.value != '') {
+      target.value = '';
+    }
+  })();
 }
 
 function clearStorage() {
