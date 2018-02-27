8f650cf7d9fdca37f523ddf74db75d18536b5766
Ignacio Villanueva <IgnaciodeNuevo@users.noreply.github.com>
2018-02-27 12:48:31 +0100

    Fixed fizzBuzz tests

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -2,26 +2,26 @@ var fizzBuzz = require('../exercises/fizzbuzz/main');
 
 describe('fizzBuzz', function() {
   it('Should return fizzbuzz string if divisible by 3 and by 5', function() {
-    expect(fizzBuzz(15)).toString('fizzbuzz');
+    expect(fizzBuzz(15)).toBe('fizzbuzz');
   });
 
   it('Should return fizz string if divisible by 3', function() {
-    expect(fizzBuzz(9)).toString('fizz');
+    expect(fizzBuzz(9)).toBe('fizz');
   });
 
   it('Should return buzz if divisible by 5', function() {
-    expect(fizzBuzz(100000)).toString('buzz');
+    expect(fizzBuzz(100000)).toBe('buzz');
   });
 
   it('Should return Fail! string if is not divisible by 3 or by 5', function() {
-    expect(fizzBuzz(0)).toString('Fail!');
+    expect(fizzBuzz(0)).toBe('Fail!');
   });
 
   it('Should return Fail! string if is not divisible by 3 or by 5', function() {
-    expect(fizzBuzz(-0)).toString('Fail!');
+    expect(fizzBuzz(-0)).toBe('Fail!');
   });
 
   it('Should return Fail! string if is not divisible by 3 or by 5', function() {
-    expect(fizzBuzz(-3)).toString('Fail!');
+      expect(fizzBuzz(-3)).toBe('fizz');
   });
 });
