ee4f712e198fb3d52041c64b6640379f4f209ff6
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2018-03-21 19:55:42 +0100

    Merged code from Adrian's review

--- exercises/todolist/main.js
+++ exercises/todolist/main.js
@@ -9,74 +9,74 @@ const api = Api;
 drawTodos(storedTodos);
 
 function drawTodos(todos) {
-    if (!todos) {
-        api.getTodos().then(todos => {
-            todoList.innerHTML = '';
-            for (let i = 0; i < todos.length; i++) {
-                todoList.innerHTML += `
+  if (!todos) {
+    api.getTodos().then(todos => {
+      todoList.innerHTML = '';
+      for (let i = 0; i < todos.length; i++) {
+        todoList.innerHTML += `
         <label>
           <input type="checkbox" data-guid=${todos[i].id}>
           ${todos[i].value}
           </label>`;
-                // SELECCIONAR JUSTO EL CHECKBOX QUE TENGA GUID TODOS[I].ID (WITH)
-                document.querySelector('input[type=checkbox]').addEventListener('change', function() {
-                    toggleTodo(todos[i].id);
-                });
-            }
+        // SELECCIONAR JUSTO EL CHECKBOX QUE TENGA GUID TODOS[I].ID (WITH)
+        document.querySelector('input[type=checkbox]').addEventListener('change', function() {
+          toggleTodo(todos[i].id);
         });
-    } else {
-        todoList.innerHTML = '';
-        for (let i = 0; i < todos.length; i++) {
-            todoList.innerHTML += `
+      }
+    });
+  } else {
+    todoList.innerHTML = '';
+    for (let i = 0; i < todos.length; i++) {
+      todoList.innerHTML += `
         <label>
           <input type="checkbox" data-guid=${todos[i].id}>
           ${todos[i].value}
         </label>`;
-            // SELECCIONAR JUSTO EL CHECKBOX QUE TENGA GUID TODOS[I].ID (WITH)
-            document.querySelector('input[type=checkbox]').addEventListener('change', function() {
-                toggleTodo(todos[i].id);
-            });
-        }
+      // SELECCIONAR JUSTO EL CHECKBOX QUE TENGA GUID TODOS[I].ID (WITH)
+      document.querySelector('input[type=checkbox]').addEventListener('change', function() {
+        toggleTodo(todos[i].id);
+      });
     }
+  }
 }
 
 function clearInput() {
-    document.getElementById('target').value = '';
+  document.getElementById('target').value = '';
 }
 
 function updateTodo(item) {
-    api.updateTodo(item).then(todos => {
-        localStorage.setItem('todoListDataBase', JSON.stringify(todos));
-        drawTodos(todos);
-        clearInput();
-    });
+  api.updateTodo(item).then(todos => {
+    localStorage.setItem('todoListDataBase', JSON.stringify(todos));
+    drawTodos(todos);
+    clearInput();
+  });
 }
 
 function toogleTodo(id) {
-    api.getTodoById(id).then(item => {
-        item.checked = !item.checked;
-        api.updateTodo(item).then(() => {
-            draw();
-        });
+  api.getTodoById(id).then(item => {
+    item.checked = !item.checked;
+    api.updateTodo(item).then(() => {
+      draw();
     });
+  });
 }
 
 function clearStorage() {
-    api.clearTodos().then(() => {
-        drawTodos();
-    });
+  api.clearTodos().then(() => {
+    drawTodos();
+  });
 }
 
 form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const value = document.getElementById('target').value;
-    if (value === '') {
-        return;
-    }
+  e.preventDefault();
+  const value = document.getElementById('target').value;
+  if (value === '') {
+    return;
+  }
 
-    api.addTodo({ checked: false, value: value }).then(todos => {
-        localStorage.setItem('todoListDataBase', JSON.stringify(todos));
-        drawTodos(todos);
-        clearInput();
-    });
+  api.addTodo({ checked: false, value: value }).then(todos => {
+    localStorage.setItem('todoListDataBase', JSON.stringify(todos));
+    drawTodos(todos);
+    clearInput();
+  });
 });
